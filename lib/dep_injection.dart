28b51d9cedd6ec43c581136cba76056f20f774e9
Diego Bermudez <158313944+diegobermudez03@users.noreply.github.com>
2024-10-22 15:55:50 -0500

    App created, token reading implemented so it displays login page or initial page depending on that

---
+++ lib/dep_injection.dart
@@ -0,0 +1,11 @@
+
+import 'package:get_it/get_it.dart';
+
+final inst = GetIt.instance;
+const uri = "localhost:3000";
+
+Future<void> initLoginDependencies() async{
+}
+
+Future<void> initAllDependencies(String token) async{
+}
---
+++ lib/features/current/pages/current_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class CurrentPage extends StatelessWidget{
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text("you are already logged"),
+    );
+  }
+}
